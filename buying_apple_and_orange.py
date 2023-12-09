673bfde82d7efca161b2f939ffbfbd14413dc8cf
Danilo Madrigalejos <wenkaru@yaho.com>
2023-12-09 15:34:02 +0800

    Added pseudocode

--- buying_apple_and_orange.py
+++ buying_apple_and_orange.py
@@ -2,4 +2,14 @@
 # Create a program that will ask how many apples and oranges you want to buy.
 # Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
 # Display the output
-s
+
+# pseudocode
+
+# - Ask user for number of apples
+# - Ask user for numbere of oranges
+# - Assign apple = 20
+# - Assign orange = 25s
+# - Compute total amount
+# - Display total amount
+
+
