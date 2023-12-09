e095dfbc97afdd4529a6b3433e03e9fbc72c8b81
Danilo Madrigalejos <wenkaru@yaho.com>
2023-12-09 15:33:19 +0800

    Added progrma statement

---
+++ buying_apple_and_orange.py
@@ -0,0 +1,5 @@
+# Program 1
+# Create a program that will ask how many apples and oranges you want to buy.
+# Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+# Display the output
+s
